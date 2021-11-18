1732d353c82b6b1d997a2238314aebe6261c495f
Moises Ocanas <moises@sapien.network>
2021-11-18 11:42:04 -0600

    adding Head component for Seo

---
+++ components/common/Head.tsx
@@ -0,0 +1,14 @@
+import NextHead from 'next/head';
+
+interface Props {
+  title: string;
+}
+
+const Head = ({ title }: Props) => (
+  <NextHead>
+    <title>Rekluto | {title}</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+  </NextHead>
+);
+
+export default Head;
