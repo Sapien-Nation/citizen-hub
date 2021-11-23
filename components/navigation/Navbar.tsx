25556aafc53382a2cb8e2a1b3d5b4a741e948027
Sabbir <prince3339@ymail.com>
2021-11-24 01:26:56 +0600

    Sign out button not clicable issue fix

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
   const router = useRouter();
 
   return (
-    <Disclosure as="nav" className="relative bg-white shadow">
+    <Disclosure as="nav" className="relative bg-white shadow z-10">
       {({ open }) => (
         <>
           <div className="max-w-6xl mx-auto py-1 px-2 sm:px-4 lg:px-8">
