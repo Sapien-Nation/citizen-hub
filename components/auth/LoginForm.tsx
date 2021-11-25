d5818f88b13be249ea52965da7648690acb64707
Moises Ocanas <moises@sapien.network>
2021-11-25 11:21:20 -0600

    password lenght validation

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -79,7 +79,16 @@ const LoginForm = () => {
             autoComplete="current-password"
             required
             className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm"
-            {...register('password')}
+            {...(register('password'),
+            {
+              validate: (value: string) => {
+                if (value?.length < 8) {
+                  return 'At least 8 characters.';
+                }
+
+                return true;
+              },
+            })}
           />
         </div>
       </div>
--- components/auth/RegisterForm.tsx
+++ components/auth/RegisterForm.tsx
@@ -197,6 +197,10 @@ const RegisterForm = () => {
             placeholder="Thisismypassword123*"
             className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm"
             {...register('password', {
+              minLength: {
+                value: 8,
+                message: 'Invalid password',
+              },
               validate: (value: string) => {
                 if (!/[a-z]/.test(value)) {
                   return 'At least one lowercase letter.';
