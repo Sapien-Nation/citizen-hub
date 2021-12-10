9ba948612340db7a3d9d66d557b3c897ac6762f8
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2021-12-10 12:54:18 -0600

    feat: small wording fix

--- components/passport/views/auth/index.tsx
+++ components/passport/views/auth/index.tsx
@@ -11,24 +11,23 @@ const FigureView = ({ linkID }: Props) => {
         <h3 className="mt-3 max-w-sm mx-auto font-bold lg:text-2xl sm:text-xl md:mt-5">
           Welcome to the Sapien Tribe!
         </h3>
-        <p className="mt-3 max-w-lg mx-auto text-md font-light py-4 text-gray-500 sm:text-xl md:mt-5">
-          Please input your favorite historical figure. Your choice of figure
-          should reflect the values you will champion as a member of our tribe.
+        <p className="mt-3 max-w-lg mx-auto text-md font-light py-4 text-gray-500 sm:text-xl md:mt-5 text-justify">
+          We are very happy to have you here!, before we can start the
+          experience of creating your unique passport, we require you to be
+          logged into your account.
         </p>
       </div>
-      <main className="lg:relative">
-        <div className="my-10">
-          <p className="text-base inline">
-            You are no logged in. Click
-            <Link href={`/login?redirect=/passport/${linkID}`}>
-              <a className="font-medium text-purple-600 hover:text-purple-500">
-                &nbsp;here&nbsp;
-              </a>
-            </Link>
-            to login.
-          </p>
-        </div>
-      </main>
+      <div className="my-10 flex justify-center">
+        <p className="text-base inline center-text">
+          Click
+          <Link href={`/login?redirect=/passport/${linkID}`}>
+            <a className="font-medium text-purple-600 hover:text-purple-500">
+              &nbsp;here&nbsp;
+            </a>
+          </Link>
+          to login.
+        </p>
+      </div>
     </>
   );
 };
