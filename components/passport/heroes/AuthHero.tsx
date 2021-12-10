aa817cd935fc8450857a206e7b69aa0416602627
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2021-12-10 12:04:10 -0600

    feat: adding Form to check link

--- components/passport/heroes/AuthHero.tsx
+++ components/passport/heroes/AuthHero.tsx
@@ -1,4 +1,5 @@
-import { ArrowRightIcon } from '@heroicons/react/outline';
+// components
+import PassportLinkForm from 'components/passport/PassportLinkForm';
 
 const AuthHero = () => {
   return (
@@ -6,12 +7,15 @@ const AuthHero = () => {
       <div className="mx-auto max-w-6xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
         <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
           <p className="mt-3 max-w-md mx-auto uppercase font-extrabold text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
-            LOREM IPSUM DOLOR SIT AMET
+            Sapien Passport
           </p>
           <h1 className="text-4xl tracking-tight font-medium text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-            <span className="block xl:inline">Lorem ipsum</span>{' '}
-            <span className="block xl:inline">dolor sit amet</span>
+            <span className="block xl:inline">Join</span>{' '}
+            <span className="block xl:inline">The new Web</span>
           </h1>
+          <div className="mt-3">
+            <PassportLinkForm />
+          </div>
         </div>
       </div>
       <div className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full flex items-center justify-center">
