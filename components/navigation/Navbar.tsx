7669f6081ff2dd7b39ecfe6db1d717f5dd60fe6a
javierocanas <javierocanas@gmail.com>
2021-12-17 16:22:13 -0600

    feat: removing not required (for now) compnents

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -18,16 +18,6 @@ import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
 // utils
 import { mergeClassNames } from 'utils/styles';
 
-const navigation = [
-  { name: 'Home', href: '/' },
-  { name: 'Passport', href: '/passport' },
-  { name: 'Protocol', href: '/protocol' },
-  { name: 'Metaverse', href: '/metaverse' },
-  { name: 'Partners', href: '/partners' },
-  { name: 'Blog', href: '/blog' },
-  { name: 'About Us', href: '/about' },
-];
-
 const Navbar = () => {
   const { me } = useAuth();
   const router = useRouter();
@@ -42,26 +32,10 @@ const Navbar = () => {
                 <div className="shrink-0 flex items-center">
                   {/* eslint-disable-next-line @next/next/no-img-element */}
                   <img
-                    className="lg:border-r border-gray-300 pr-6 w-20"
+                    className="pr-6 w-20"
                     src="/images/logo.png"
                     alt="sapien"
                   />
-                </div>
-                <div className="hidden lg:ml-6 lg:flex lg:space-x-8">
-                  {navigation.map((nav) => (
-                    <Link key={nav.href} href={nav.href}>
-                      <a
-                        className={mergeClassNames(
-                          router.asPath === nav.href
-                            ? 'text-gray-900'
-                            : 'text-gray-500 hover:text-gray-700',
-                          'inline-flex items-center px-1 pt-1 text-sm font-medium'
-                        )}
-                      >
-                        {nav.name}
-                      </a>
-                    </Link>
-                  ))}
                 </div>
               </div>
               <div className="flex items-center lg:hidden">
@@ -152,23 +126,6 @@ const Navbar = () => {
           </div>
 
           <Disclosure.Panel className="lg:hidden">
-            <div className="pt-2 pb-3 space-y-1">
-              {navigation.map((nav) => (
-                <Disclosure.Button
-                  as="a"
-                  href={nav.href}
-                  key={nav.href}
-                  className={mergeClassNames(
-                    router.asPath === nav.href
-                      ? 'bg-purple-50 border-purple-500 text-purple-700'
-                      : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800',
-                    'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
-                  )}
-                >
-                  {nav.name}
-                </Disclosure.Button>
-              ))}
-            </div>
             <div className="pt-4 pb-3 border-t border-gray-200">
               {me ? (
                 <>
