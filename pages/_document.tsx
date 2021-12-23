e1ebf01441a1d109844cb4513f7ce652a936e936
javierocanas <javierocanas@gmail.com>
2021-12-23 11:21:59 -0600

    feat: google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,23 @@ class MyDocument extends Document {
     return (
       <Html className="f-full">
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
             rel="stylesheet"
