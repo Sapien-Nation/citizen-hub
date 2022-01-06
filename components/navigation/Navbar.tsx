113c5e199095a7b07a6f4092cb36ca8515d50a1d
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-01-06 16:28:03 -0600

    feat: Adding Team page

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -18,7 +18,10 @@ import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
 // utils
 import { mergeClassNames } from 'utils/styles';
 
-const navigation = [{ name: 'Passport', href: '/passport' }];
+const navigation = [
+  { name: 'Passport', href: '/passport', visible: false },
+  { name: 'Team', href: '/team', visible: true },
+];
 
 const Navbar = () => {
   const { me } = useAuth();
@@ -45,18 +48,22 @@ const Navbar = () => {
                 </div>
                 <div className="hidden lg:ml-6 lg:flex lg:space-x-8">
                   {navigation.map((nav) => (
-                    <Link key={nav.href} href={nav.href}>
-                      <a
-                        className={mergeClassNames(
-                          router.asPath === nav.href
-                            ? 'text-gray-900'
-                            : 'text-gray-500 hover:text-gray-700',
-                          'inline-flex items-center px-1 pt-1 text-sm font-medium'
-                        )}
-                      >
-                        {nav.name}
-                      </a>
-                    </Link>
+                    <>
+                      {nav.visible && (
+                        <Link key={nav.href} href={nav.href}>
+                          <a
+                            className={mergeClassNames(
+                              router.asPath === nav.href
+                                ? 'text-gray-900'
+                                : 'text-gray-500 hover:text-gray-700',
+                              'inline-flex items-center px-1 pt-1 text-sm font-medium'
+                            )}
+                          >
+                            {nav.name}
+                          </a>
+                        </Link>
+                      )}
+                    </>
                   ))}
                 </div>
               </div>
@@ -160,19 +167,23 @@ const Navbar = () => {
           <Disclosure.Panel className="lg:hidden">
             <div className="pt-2 pb-3 space-y-1">
               {navigation.map((nav) => (
-                <Disclosure.Button
-                  as="a"
-                  href={nav.href}
-                  key={nav.href}
-                  className={mergeClassNames(
-                    router.asPath === nav.href
-                      ? 'bg-purple-50 border-purple-500 text-purple-700'
-                      : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800',
-                    'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                <>
+                  {nav.visible && (
+                    <Disclosure.Button
+                      as="a"
+                      href={nav.href}
+                      key={nav.href}
+                      className={mergeClassNames(
+                        router.asPath === nav.href
+                          ? 'bg-purple-50 border-purple-500 text-purple-700'
+                          : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800',
+                        'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                      )}
+                    >
+                      {nav.name}
+                    </Disclosure.Button>
                   )}
-                >
-                  {nav.name}
-                </Disclosure.Button>
+                </>
               ))}
             </div>
             <div className="pt-4 pb-3 border-t border-gray-200">
---
+++ pages/team.tsx
@@ -0,0 +1,8 @@
+// types
+import { NextPage } from 'next';
+
+const TeamPage: NextPage = () => {
+  return <h1>TEAM page</h1>;
+};
+
+export default TeamPage;
