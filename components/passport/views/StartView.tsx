7d56d2c01e636c7daf5955aab1bb2271ec87a611
javierocanas <javierocanas@gmail.com>
2022-01-07 14:25:24 -0600

    feat: sapien theme

--- components/passport/views/StartView.tsx
+++ components/passport/views/StartView.tsx
@@ -13,7 +13,7 @@ const StartView = ({ setPassportView }: Props) => {
     <div className="w-full h-full flex items-center justify-center">
       <div className="lg:relative">
         <div className="w-full text-center">
-          <h2 className="text-5xl font-extrabold mb-12 skew-y-3">
+          <h2 className="text-5xl font-extrabold font-sans mb-12 skew-y-3">
             <span className="underline decoration-4 decoration-purple-600 text-7xl">
               welcome to
             </span>{' '}
@@ -21,7 +21,7 @@ const StartView = ({ setPassportView }: Props) => {
           </h2>
           <button
             onClick={() => setPassportView(PassportViews.Figure)}
-            className="animate-bounce py-2 px-8 border border-transparent rounded-md shadow-sm text-xl font-bold text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+            className="animate-bounce py-2 px-8 border border-transparent rounded-md shadow-sm text-xl font-bold text-white bg-sapien-purple hover:bg-sapien-purple-light focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
           >
             Start
           </button>
