95a46e5320e8e962d5a713facd957f208b095b87
Moises Ocanas <moises@sapien.network>
2022-01-06 18:43:05 -0600

    feat: fixes on team page

--- pages/team.tsx
+++ pages/team.tsx
@@ -18,37 +18,37 @@ const people = [
     name: 'Teja',
     role: 'CCO',
     imageUrl: '/images/team/Teja.png',
-    bio: 'Founding member of the Sapien Tribe, Chief Communications Officer of the Sapien Network. Teja is dedicated to the learning process and seeks to foster curiosity in himself and others. Teja earned his BS in Computer Engineering from UC Davis in 2017. Prior to joining the Sapien Tribe, Teja was a graduate student in Computer Engineering at UC Davis, before taking a leave of absence. He has previously worked at Tesla, Xpeng Motors, and Lyft on projects in robotics and the autonomous vehicle space. Raised in Colorado, he seeks to find a synthesis between technology and nature.',
+    bio: `Founding member of the Sapien Tribe, Chief Communications Officer of the Sapien Network. Teja is dedicated to the learning process and seeks to foster curiosity in himself and others. Teja earned his <span class='underline decoration-indigo-500 decoration-2 text-gray-900 font-medium'>BS in Computer Engineering from UC Davis in 2017</span>. Prior to joining the Sapien Tribe, Teja was a graduate student in Computer Engineering at UC Davis, before taking a leave of absence. He has previously <span class='underline decoration-indigo-500 decoration-2 text-gray-900 font-medium'>worked at Tesla, Xpeng Motors, and Lyft</span> on projects in robotics and the autonomous vehicle space. Raised in Colorado, he seeks to find a synthesis between technology and nature.`,
   },
   {
     name: 'Greg',
     role: 'CTO',
     imageUrl: '/images/team/Ankit.png',
-    bio: 'Chief Technology Officer at Sapien Network. In Greg’s endeavors throughout his career, he has strived to change the status quo, to make it better, easier to understand, accessible by everyone and, importantly, decentralized. Greg approaches complexities of life and systems, and distills them into beautiful solutions (“there is elegance in simplicity” – Greg). Astrophysics, Computer Science and Mathematics form the basis of his academic learnings. His patent in peer to peer networks, extensive work in cryptography, and his gaming technologies form the basis of his beliefs and support of the crypto world. Bringing his learnings, Greg joined the Sapien tribe to contribute to create a new, better place for humans.',
+    bio: `Chief Technology Officer at Sapien Network. In Greg’s endeavors throughout his career, he has strived to change the status quo, to make it better, easier to understand, accessible by everyone and, importantly, decentralized. Greg approaches complexities of life and systems, and distills them into beautiful solutions (“there is elegance in simplicity” – Greg). <span class='underline decoration-indigo-500 decoration-2 text-gray-900 font-medium'>Astrophysics, Computer Science and Mathematics form the basis of his academic learnings</span>. His patent in peer to peer networks, extensive work in cryptography, and his gaming technologies form the basis of his beliefs and support of the crypto world. Bringing his learnings, Greg joined the Sapien tribe to contribute to create a new, better place for humans.`,
   },
   {
     name: 'Latif',
     role: 'COO',
     imageUrl: '/images/team/Latif.png',
-    bio: 'Latif is the Founding investor and COO for the Sapien Network. Latif’s attraction to adventure and the unknown started at 18 when he traveled across the world from India with simple change in his pocket. After completing his undergraduate studies in Computer Science at the University of Arizona, Latif took another risk and became a software engineer at a “not very popular” company in the Pacific Northwest named Microsoft. Latif’s tenure at Microsoft lasted nearly 16 cumulative years, finally ending up as the General Manager of the Unlimited Potential group, leading a team in India to bring benefits of technology to emerging markets. He has also been involved in many other large projects like co-founding eMemories, helping launch Norton360 for Symantec, and most recently as the managing director for eBay India.',
+    bio: `Latif is the Founding investor and COO for the Sapien Network. Latif’s attraction to adventure and the unknown started at 18 when he traveled across the world from India with simple change in his pocket. After completing his undergraduate studies in <span class='underline decoration-indigo-500 decoration-2 text-gray-900 font-medium'>Computer Science at the University of Arizona</span>, Latif took another risk and became a software engineer at a “not very popular” company in the Pacific Northwest named Microsoft. <span class='underline decoration-indigo-500 decoration-2 text-gray-900 font-medium'>Latif’s tenure at Microsoft lasted nearly 16 cumulative years, finally ending up as the General Manager of the Unlimited Potential group</span>, leading a team in India to bring benefits of technology to emerging markets. He has also been involved in many other large projects like <span class='underline decoration-indigo-500 decoration-2 text-gray-900 font-medium'>co-founding eMemories, helping launch Norton360 for Symantec</span>, and most recently as the managing director for eBay India.`,
   },
   {
     name: 'Hash',
     role: 'CMO',
     imageUrl: '/images/team/Hash.png',
-    bio: "Hash leads Sapien's growing marketing team as the CMO. He brings natural marketing talent as well as knowledge from his experience with both B2B and B2C marketing. As a published poet, passionate writer, musician, and graphic artist, he brings the level of creative direction needed to communicate Sapien's vision to the world. As an immigrant raised in a low-income household, Hash is passionate about leveraging emerging technologies to seek justice for underserved communities.",
+    bio: `Hash leads Sapien's growing marketing team as the CMO. He brings natural marketing talent as well as knowledge from his experience with both B2B and B2C marketing. As a published poet, passionate writer, musician, and graphic artist, he brings the level of creative direction needed to communicate Sapien's vision to the world. As an immigrant raised in a low-income household, Hash is passionate about leveraging emerging technologies to seek justice for underserved communities.`,
   },
   {
     name: 'Missy',
     role: 'CSO',
     imageUrl: '/images/team/Ankit.png',
-    bio: 'As Chief Sustainability Officer, Missy leads Sapien’s community building and social/environmental impact efforts. Before Sapien, Missy helped lead business development for multiple startups - a circular economy platform, Loop, and a market research firm, Vancery. She also was the VP of Member Development for the environmental nonprofit, BEACN, and managed the first global onboarding mentorship program at Tesla. In her free time, she mentors college students on their climate action projects and supports sustainable food communities. She graduated from UC Berkeley with a B.S. in Society and Environment, concentrating in US Environmental Policy and Management.',
+    bio: `As Chief Sustainability Officer, Missy leads Sapien’s community building and social/environmental impact efforts. Before Sapien, Missy helped lead business development for multiple startups - a circular economy platform, Loop, and a market research firm, Vancery. She also was the <span class='underline decoration-indigo-500 decoration-2 text-gray-900 font-medium'>VP of Member Development for the environmental nonprofit, BEACN, and managed the first global onboarding mentorship program at Tesla</span>. In her free time, she mentors college students on their climate action projects and supports sustainable food communities. She <span class='underline decoration-indigo-500 decoration-2 text-gray-900 font-medium'>graduated from UC Berkeley with a B.S. in Society and Environment</span>, concentrating in US Environmental Policy and Management.`,
   },
   {
     name: 'Lynn',
     role: 'Advisor',
     imageUrl: '/images/team/Hash.png',
-    bio: 'Lynn has previously advised for Lykke Corporation, cryptocurrency exchange, compliance, risk management, ALM, trading strategies, ICO token structuring & due diligence. Chief Compliance Officer and Director of Research for Fossil Free Indexes (FFI), and structuring of investment fund strategies. +25 years in both industry and advisory services, working for firms such as Merrill Lynch, JP Morgan Chase, and Deloitte. Advisory services to global financial institutions, asset managers, and funds in compliance, risk management, regulatory requirements, portfolio analysis, strategies, and valuation. Co-author whitepaper “The Missing Link: How a Distributed Ledger System Can Support Global Climate Action.” Published in the areas of asset management, risk management and valuation. Series 65 License, Uniform Investment Advisor Law Examination. B.S. in Economics Wharton, University of Pennsylvania.',
+    bio: `Lynn has previously advised for Lykke Corporation, cryptocurrency exchange, compliance, risk management, ALM, trading strategies, ICO token structuring & due diligence. Chief Compliance Officer and Director of Research for Fossil Free Indexes (FFI), and structuring of investment fund strategies. +25 years in both industry and advisory services, <span class='underline decoration-indigo-500 decoration-2 text-gray-900 font-medium'>working for firms such as Merrill Lynch, JP Morgan Chase, and Deloitte</span>. Advisory services to global financial institutions, asset managers, and funds in compliance, risk management, regulatory requirements, portfolio analysis, strategies, and valuation. Co-author whitepaper “The Missing Link: How a Distributed Ledger System Can Support Global Climate Action.” Published in the areas of asset management, risk management and valuation. Series 65 License, Uniform Investment Advisor Law Examination. <span class='underline decoration-indigo-500 decoration-2 text-gray-900 font-medium'>B.S. in Economics Wharton, University of Pennsylvania</span>.`,
   },
 ];
 
@@ -63,26 +63,29 @@ const TeamPage: NextPage = () => {
 
           <ul
             role="list"
-            className="space-y-12 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8 lg:gap-y-12 lg:space-y-0"
+            className="space-y-12 lg:grid lg:grid-cols-1 lg:items-start lg:gap-x-8 lg:gap-y-12 lg:space-y-0"
           >
             {people.map((person) => (
               <li key={person.name}>
                 <div className="space-y-4 sm:grid sm:grid-cols-3 sm:gap-6 sm:space-y-0 lg:gap-8">
-                  <div className="h-0 aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
+                  <div className="h-0 aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-3">
                     <img
-                      className="object-cover shadow-lg rounded-lg"
+                      className="object-cover h-10 shadow-lg rounded-lg"
                       src={person.imageUrl}
                       alt=""
                     />
                   </div>
                   <div className="sm:col-span-2">
-                    <div className="space-y-4">
+                    <div className="space-y-4 flex flex-col justify-center h-full">
                       <div className="text-lg leading-6 font-medium space-y-1">
                         <h3>{person.name}</h3>
                         <p className="text-indigo-600">{person.role}</p>
                       </div>
                       <div className="text-lg">
-                        <p className="text-gray-500">{person.bio}</p>
+                        <p
+                          className="text-gray-500"
+                          dangerouslySetInnerHTML={{ __html: person.bio }}
+                        />
                       </div>
                     </div>
                   </div>
