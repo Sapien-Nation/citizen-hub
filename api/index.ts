6a8aba3e8ce967bb0b5bff435cc88fcfbbe8fdfd
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-01-18 10:27:17 -0500

    feat: ENVs from JS

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
+const host = process.browser ? window.location.host : '';
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL:
+    host === 'passport-sandbox.sapien.network'
+      ? 'https://token-sandbox.sapien.network/'
+      : 'https://token-poc.sapien.network/',
 });
 
 export const authInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_AUTH_URL,
+  baseURL:
+    host === 'passport-sandbox.sapien.network'
+      ? 'https://sandbox-oauth.sapien.network/'
+      : 'https://oauth.sapien.network/ ',
 });
 
 instance.interceptors.request.use((config) => {
