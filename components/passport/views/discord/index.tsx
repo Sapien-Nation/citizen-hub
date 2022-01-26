58fd2f0dd880c76d1328231b6601dcd2d891c406
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-01-26 13:59:58 -0600

    feat: Adding description for pending views

--- components/passport/views/discord/index.tsx
+++ components/passport/views/discord/index.tsx
@@ -1,5 +1,10 @@
 const Discord = () => {
-  return <h1>TODO Discord</h1>;
+  return (
+    <h1>
+      on this view we need to show a nice message to invite the user to join the
+      Discord server
+    </h1>
+  );
 };
 
 export default Discord;
