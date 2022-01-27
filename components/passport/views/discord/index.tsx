62f27c1b3b25177ebc0841efcb515cc4a2caade0
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-01-26 19:43:27 -0600

    feat: update discord welcome page

--- components/passport/views/discord/index.tsx
+++ components/passport/views/discord/index.tsx
@@ -4,6 +4,9 @@ const Discord = () => {
       The confirmation message is: Congratulations, you have selected [NAME
       SELECTION] as your avatar in the Sapien Tribe! Join our Discord to keep up
       to date on the latest news for passport holders.
+      <br />
+      Please join our community and add yourself to the whitelist to secure your
+      historical pick.
     </h1>
   );
 };
