cdc91985364dee0d5ef7752c0b9f1857ae9c31b1
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-03-03 19:42:53 -0600

    feat: Steps for Avatar

--- components/passport/views/avatar/index.tsx
+++ components/passport/views/avatar/index.tsx
@@ -12,18 +12,18 @@ import { Query } from 'components/common';
 import { mergeClassNames } from 'utils/styles';
 
 interface Props {
-  linkID: string;
+  avatar: { id: string };
 }
 
-const HistoricalFiguresSearch = ({ linkID }: Props) => {
-  const [avatar, setAvatar] = useState<string | null>(null);
+const HistoricalFiguresSearch = ({ avatar }: Props) => {
   const [isFetching, setIsFetching] = useState(false);
 
   const toast = useToast();
 
-  const handleContinue = async () => {
+  const handleCreatePassport = async () => {
     setIsFetching(true);
     try {
+      // TODO
     } catch (error) {
       toast({
         message: error,
@@ -42,72 +42,18 @@ const HistoricalFiguresSearch = ({ linkID }: Props) => {
       </div>
       <main className="lg:relative">
         <div className="mx-auto max-w-6xl w-full pt-16 px-4 xl:px-0">
-          <ul
-            role="list"
-            className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-5 xl:gap-x-8"
-          >
-            <Query
-              api="/api/v3/passport/avatar"
-              options={{
-                fetcher: () => [
-                  'https://via.placeholder.com/150?id=0',
-                  'https://via.placeholder.com/150?id=1',
-                  'https://via.placeholder.com/150?id=2',
-                  'https://via.placeholder.com/150?id=3',
-                  'https://via.placeholder.com/150?id=4',
-                  'https://via.placeholder.com/150?id=5',
-                ],
-              }}
-            >
-              {(data: Array<string>) => (
-                <>
-                  {data.map((image) => (
-                    <li
-                      className="relative transition ease-in-out"
-                      key={image}
-                      onClick={() => setAvatar(image)}
-                    >
-                      <div
-                        className={mergeClassNames(
-                          image === avatar ? 'ring-2 ring-indigo-500' : '',
-                          'relative group flex cursor-pointer justify-center items-center w-full aspect-w-10 h-56 aspect-h-7 rounded-lg bg-gray-100 overflow-hidden after:absolute hover:after:sm:bg-black hover:after:sm:bg-opacity-30 hover:after:w-full hover:after:h-full hover:after:top-0'
-                        )}
-                      >
-                        {image === avatar && (
-                          <CheckCircleIcon className="h-5 w-5 text-purple-900 absolute right-2 top-2 left-auto z-10 drop-shadow-lg" />
-                        )}
-                        <Image
-                          alt={'Avatar Image result for the selected figure'}
-                          src={image}
-                          layout="fill"
-                          objectFit="cover"
-                          quality={100}
-                          className="object-cover h-full w-full pointer-events-none group-hover:opacity-75"
-                          onError={(event) => {
-                            (event.target as HTMLImageElement).src =
-                              'https://d151dmflpumpzp.cloudfront.net/images/tribes/default_temp.jpeg';
-                          }}
-                        />
-                      </div>
-                    </li>
-                  ))}
-                </>
-              )}
-            </Query>
-          </ul>
+          <div>TODO show Avatar here</div>
           <div className="mt-20 sticky bottom-10 flex flex-col justify-center items-center">
-            {Boolean(avatar) && (
-              <button
-                disabled={isFetching}
-                type="button"
-                className={`inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-sapien hover:bg-sapien-80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 ${
-                  isFetching ? 'animate-pulse' : ''
-                }`}
-                onClick={handleContinue}
-              >
-                Generate Passport
-              </button>
-            )}
+            <button
+              disabled={isFetching}
+              type="button"
+              className={`inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-sapien hover:bg-sapien-80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 ${
+                isFetching ? 'animate-pulse' : ''
+              }`}
+              onClick={handleCreatePassport}
+            >
+              Generate Passport
+            </button>
           </div>
         </div>
       </main>
