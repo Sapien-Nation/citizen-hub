72cf0d098319d29e234539dc1e67c801de9226ad
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-04-18 11:47:17 -0500

    feat: no call mint check if no logged in

--- components/common/Banner.tsx
+++ components/common/Banner.tsx
@@ -1,6 +1,11 @@
 import Link from 'next/link';
-import { SpeakerphoneIcon, XIcon } from '@heroicons/react/outline';
+import { SpeakerphoneIcon } from '@heroicons/react/outline';
+
+// components
 import { Query } from 'components/common';
+
+// context
+import { useAuth } from 'context/user';
 
 interface IBanner {
   title: string;
@@ -10,6 +15,8 @@ interface IBanner {
 
 const ProtocolURL = process.env.NEXT_PUBLIC_PROTOCOL_URL;
 export default function Banner({ title, message, action }: IBanner) {
+  const { me } = useAuth();
+
   const renderView = (code: number) => {
     const handleRedirectToProtocol = (path = '') => {
       const getURL = () => {
@@ -76,19 +83,18 @@ export default function Banner({ title, message, action }: IBanner) {
             </p>
           </div>
           <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-            <Query api="/passport-api/mint-checker">
-              {({ code }: { code: number | null }) => renderView(code)}
-            </Query>
+            {me === null ? (
+              <Link href="/passport/purchase">
+                <a className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-extrabold text-indigo-600 bg-white hover:bg-indigo-50">
+                  Purchase Passport
+                </a>
+              </Link>
+            ) : (
+              <Query api="/passport-api/mint-checker">
+                {({ code }: { code: number | null }) => renderView(code)}
+              </Query>
+            )}
           </div>
-          {/* <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
-            <button
-              type="button"
-              className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
-            >
-              <span className="sr-only">Dismiss</span>
-              <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
-            </button>
-          </div> */}
         </div>
       </div>
     </div>
