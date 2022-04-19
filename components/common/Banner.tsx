b59c65c40ee8794dcc0916afd53a7d5b30b33713
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-04-19 14:31:07 -0500

    feat: small change on buttons

--- components/common/Banner.tsx
+++ components/common/Banner.tsx
@@ -84,20 +84,11 @@ export default function Banner({ title, message, action }: IBanner) {
           </div>
           <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
             {me === null ? (
-              <>
-                <Link href="/passport/purchase">
-                  <a className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-extrabold text-indigo-600 bg-white hover:bg-indigo-50">
-                    Purchase Passport
-                  </a>
-                </Link>
-
-                <button
-                  onClick={() => handleRedirectToProtocol()}
-                  className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-extrabold text-indigo-600 bg-white hover:bg-indigo-50"
-                >
-                  Go to the Protocol
-                </button>
-              </>
+              <Link href="/passport/purchase">
+                <a className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-extrabold text-indigo-600 bg-white hover:bg-indigo-50">
+                  Purchase Passport
+                </a>
+              </Link>
             ) : (
               <Query api="/passport-api/mint-checker" ignoreError>
                 {({ code }: { code: number | null }) => renderView(code)}
