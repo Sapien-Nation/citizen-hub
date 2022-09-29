c3dc61e3b509340623d901f97f1c4cca89e2f212
Ankit <ankit@sapien.network>
2022-09-28 22:01:23 -0700

    add Features

--- pages/index.tsx
+++ pages/index.tsx
@@ -406,10 +406,10 @@ const IndexPage: NextPage = () => {
                       </div>
                     </div>
                     <div className="ml-4">
-                      <h5 className="text-2xl leading-6 text-gray-900 dark:text-white font-bold leading-7 md:w-[28rem]">
+                      <h5 className="pl-2 text-2xl leading-6 text-gray-900 dark:text-white font-bold leading-7 md:w-[28rem]">
                         Collaborate in committees
                       </h5>
-                      <p className="mt-2 text-lg font-semibold leading-6 text-gray-500 dark:text-gray-300">
+                      <p className="pl-2 mt-2 text-lg font-semibold leading-6 text-gray-500 dark:text-gray-300">
                         From nation-building to technology, find your tribe of
                         changemakers in the Sapien Nation.
                       </p>
@@ -436,10 +436,10 @@ const IndexPage: NextPage = () => {
                       </div>
                     </div>
                     <div className="ml-4">
-                      <h5 className="text-2xl leading-6 text-gray-900 dark:text-white font-bold leading-7 md:w-[28rem]">
+                      <h5 className="pl-2 text-2xl leading-6 text-gray-900 dark:text-white font-bold leading-7 md:w-[28rem]">
                         Submit proposals
                       </h5>
-                      <p className="mt-2 text-lg font-semibold leading-6 text-gray-500 dark:text-gray-300">
+                      <p className="pl-2 mt-2 text-lg font-semibold leading-6 text-gray-500 dark:text-gray-300">
                         Participate in community discussions and lead
                         initiatives that resonate with you.
                       </p>
@@ -489,10 +489,10 @@ const IndexPage: NextPage = () => {
                       </div>
                     </div>
                     <div className="ml-4">
-                      <h5 className="text-2xl leading-6 text-gray-900 dark:text-white font-bold leading-7 md:w-[28rem]">
+                      <h5 className="pl-2 text-2xl leading-6 text-gray-900 dark:text-white font-bold leading-7 md:w-[28rem]">
                         Have your project invested in
                       </h5>
-                      <p className="mt-2 text-lg font-semibold leading-6 text-gray-500 dark:text-gray-300">
+                      <p className="pl-2 mt-2 text-lg font-semibold leading-6 text-gray-500 dark:text-gray-300">
                         Rally the Sapien Nation to get behind your proposals and
                         fund your dream projects.
                       </p>
@@ -539,129 +539,89 @@ const IndexPage: NextPage = () => {
           </div>
         </div>
 
-        <div className="container mx-auto px-6 p-6 bg-white bg-gradient-to-r from-sapien to-sapien-blue-100 rounded-4xl mt-12">
-          <div className="mb-16 text-center">
-            <h2 className="text-base text-grey font-semibold tracking-wide uppercase ">
-              Features
-            </h2>
-            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
-              A Social Network of DAOs, powered by the Sapien Passport.
-            </p>
-          </div>
-          <div className="flex flex-wrap my-12 dark:text-white">
-            <div className="w-full border-b md:w-1/2 md:border-r lg:w-1/3 p-8">
-              <div className="flex items-center mb-6">
-                <svg
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="h-6 w-6 text-indigo-500"
-                  viewBox="0 0 1792 1792"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z"></path>
-                </svg>
-                <div className="ml-4 text-xl">Increase sales</div>
-              </div>
-              <p className="leading-loose text-gray-500 dark:text-gray-200 text-md">
-                Receive more sales by selling across multple sales channels
-                instead of just having a single point of entry.
+        <div className="container mx-auto px-2 p-2 border-solid bg-white bg-gradient-to-r from-sapien to-sapien-blue-100 rounded-4xl mt-12">
+          <div className="container mx-auto border-solid bg-neutral-900 rounded-4xl">
+            <div className="mb-8 text-center">
+              <h2 className="text-base text-grey font-semibold tracking-wide uppercase text-gray-300 pt-5">
+                Sapien Network
+              </h2>
+              <p className="mt-4 text-4xl leading-10 font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+                A Social Network of DAOs, powered by an NFT Passport.
               </p>
             </div>
-            <div className="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r p-8">
-              <div className="flex items-center mb-6">
-                <svg
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="h-6 w-6 text-indigo-500"
-                  viewBox="0 0 1792 1792"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z"></path>
-                </svg>
-                <div className="ml-4 text-xl">Overlays</div>
+            <div className="flex flex-wrap dark:text-white">
+              <div className="w-full border-b md:w-1/2 md:border-r lg:w-1/3 p-8 border-slate-600">
+                <div className="flex items-center mb-6">
+                  <img alt="" src="/images/dapp.png" className="h-7 pr-3" />
+                  <div className="text-xl font-semibold">Onboard Easily</div>
+                </div>
+                <p className="leading-loose text-gray-500 dark:text-gray-200 text-lg">
+                  Everyone gets an automagical Web3 wallet. No prior crypto
+                  experience needed.
+                </p>
               </div>
-              <p className="leading-loose text-gray-500 dark:text-gray-200 text-md">
-                Apply beautiful overlays to every product image distributed
-                through our platform. A visual touch.
-              </p>
-            </div>
-            <div className="w-full border-b md:w-1/2 md:border-r lg:w-1/3 lg:border-r-0 p-8">
-              <div className="flex items-center mb-6">
-                <svg
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="h-6 w-6 text-indigo-500"
-                  viewBox="0 0 1792 1792"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z"></path>
-                </svg>
-                <div className="ml-4 text-xl">Control</div>
+              <div className="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r p-8 border-slate-600">
+                <div className="flex items-center mb-6">
+                  <img alt="" src="/images/dapp.png" className="h-7 pr-3" />
+                  <div className="text-xl font-semibold">Find Your Tribes</div>
+                </div>
+                <p className="leading-loose text-gray-500 dark:text-gray-200 text-lg">
+                  Join or create a new community. When you&apos;re ready,
+                  upgrade it to a DAO.
+                </p>
               </div>
-              <p className="leading-loose text-gray-500 dark:text-gray-200 text-md">
-                Apply filters and control which products to sell on each sales
-                channel. E.g. exclude products with low margins.
-              </p>
-            </div>
-            <div className="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r lg:border-b-0 p-8">
-              <div className="flex items-center mb-6">
-                <svg
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="h-6 w-6 text-indigo-500"
-                  viewBox="0 0 1792 1792"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z"></path>
-                </svg>
-                <div className="ml-4 text-xl">Mapping</div>
+              <div className="w-full border-b md:w-1/2 md:border-r lg:w-1/3 lg:border-r-0 p-8 border-slate-600">
+                <div className="flex items-center mb-6">
+                  <img alt="" src="/images/dapp.png" className="h-7 pr-3" />
+                  <div className="text-xl font-semibold">
+                    Issue Badges ({' '}
+                    <a
+                      className="network-state"
+                      href="https://www.coindesk.com/learn/what-are-soulbound-tokens-the-non-transferrable-nft-explained/"
+                      target="_blank" rel="noreferrer"
+                    >
+                      SBTs
+                    </a>{' '}
+                    )
+                  </div>
+                </div>
+                <p className="leading-loose text-gray-500 dark:text-gray-200 text-lg">
+                  DAOs get a Safe to create badges, tokens that build your
+                  social ledger on Sapien.
+                </p>
               </div>
-              <p className="leading-loose text-gray-500 dark:text-gray-200 text-md">
-                Map product categories with each sales channels&#x27; own
-                categories and achieve better results and lower costs.
-              </p>
-            </div>
-            <div className="w-full border-b md:w-1/2 md:border-r md:border-b-0 lg:w-1/3 lg:border-b-0 p-8">
-              <div className="flex items-center mb-6">
-                <svg
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="h-6 w-6 text-indigo-500"
-                  viewBox="0 0 1792 1792"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z"></path>
-                </svg>
-                <div className="ml-4 text-xl">Fill the missing</div>
+              <div className="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r lg:border-b-0 p-8 border-slate-600">
+                <div className="flex items-center mb-6">
+                  <img alt="" src="/images/dapp.png" className="h-7 pr-3" />
+                  <div className="text-xl font-semibold">Rooms & Channels</div>
+                </div>
+                <p className="leading-loose text-gray-500 dark:text-gray-200 text-lg">
+                  Badges can be used to gate chat rooms or empower contributors
+                  for channel posts.
+                </p>
               </div>
-              <p className="leading-loose text-gray-500 dark:text-gray-200 text-md">
-                Modify products with extra properties and achieve the maximum
-                output for each installed sales channel.
-              </p>
-            </div>
-            <div className="w-full md:w-1/2 lg:w-1/3 p-8">
-              <div className="flex items-center mb-6">
-                <svg
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="h-6 w-6 text-indigo-500"
-                  viewBox="0 0 1792 1792"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z"></path>
-                </svg>
-                <div className="ml-4 text-xl">Dynamic Texts</div>
+              <div className="w-full border-b md:w-1/2 md:border-r md:border-b-0 lg:w-1/3 lg:border-b-0 p-8 border-slate-600">
+                <div className="flex items-center mb-6">
+                  <img alt="" src="/images/dapp.png" className="h-7 pr-3" />
+                  <div className="text-xl font-semibold">
+                    Autonomous Governance
+                  </div>
+                </div>
+                <p className="leading-loose text-gray-500 dark:text-gray-200 text-lg">
+                  Badges can gate voting on Snapshot or can even be issued based
+                  on the results of poll.
+                </p>
               </div>
-              <p className="leading-loose text-gray-500 dark:text-gray-200 text-md">
-                Build unique product titles and descriptions instead of spending
-                days manually editing each product.
-              </p>
+              <div className="w-full md:w-1/2 lg:w-1/3 p-8 border-slate-600">
+                <div className="flex items-center mb-6">
+                  <img alt="" src="/images/dapp.png" className="h-7 pr-3" />
+                  <div className="text-xl font-semibold">Scale Your Impact</div>
+                </div>
+                <p className="leading-loose text-gray-500 dark:text-gray-200 text-lg">
+                  Badges enable DAOs to tap into network effects. Scale, grow,
+                  and operate effortlessly.
+                </p>
+              </div>
             </div>
           </div>
         </div>
